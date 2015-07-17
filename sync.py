8045f2ee4064676b9696721c63c72d226bed5767
Hugo Peixoto <hugo.peixoto@gmail.com>
2015-07-17 21:19:14 +0100

    Adds docstrings

--- sync.py
+++ sync.py
@@ -3,6 +3,11 @@ import pip
 exceptions = ['pip', 'setuptools', 'wheel']
 
 def diff(requirements, installed):
+    """
+    Calculate which modules should be installed or uninstalled,
+    given a set of requirements and a list of installed modules.
+    """
+
     requirements = { r.req.key: r for r in requirements }
 
     to_be_installed = set()
@@ -28,6 +33,10 @@ def diff(requirements, installed):
 
 
 def sync(to_be_installed, to_be_uninstalled):
+    """
+    Install and uninstalls the given sets of modules.
+    """
+
     if to_be_uninstalled:
         pip.main(["uninstall", '-y'] + [str(req) for req in to_be_uninstalled])
 
@@ -35,6 +44,11 @@ def sync(to_be_installed, to_be_uninstalled):
         pip.main(["install"] + [str(req) for req in to_be_installed])
 
 def run():
+    """
+    Synchronize current environment by installing modules that match
+    requirements.txt and uninstalling the ones that do not match.
+    """
+
     requirements = pip.req.parse_requirements('requirements.txt', session=True)
     installed = pip.get_installed_distributions()
 
